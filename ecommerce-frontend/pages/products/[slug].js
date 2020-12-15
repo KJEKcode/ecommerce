a2c9aaf6619362bfb0ebaab76a3af35eb19d7268
kurtis kovel <kurtiskovel@gmail.com>
2020-12-14 21:19:01 -0500

    README 12/14/2020 (2) connected nextjs and strapi

--- ecommerce-frontend/pages/products/[slug].js
+++ ecommerce-frontend/pages/products/[slug].js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
-import products from '../../products.json'
-import { fromImageToUrl } from '../../utils/urls'
+import { fromImageToUrl, API_URL } from '../../utils/urls'
 import { twoDecimals } from '../../utils/format'
-const product = products[0]
 
-const Product = () => {
+const Product = ({ product }) => {
     return (
         <div>
             <Head>
@@ -24,6 +22,30 @@ const Product = () => {
             </p>
         </div>
     )
+}
+
+export async function getStaticProps({ params: { slug } }) {
+    const product_res = await fetch(`${API_URL}/products/?slug=${slug}`)
+    const found = await product_res.json()
+    return {
+        props: {
+            product: found[0] //because the API response for filters is an array
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    //retrieve all the possible paths
+    const products_res = await fetch(`${API_URL}/products/`)
+    const products = await products_res.json()
+
+    //return them to nextjs context
+    return {
+        paths: products.map(product => ({
+            params: { slug: String(product.slug) }
+        })),
+        fallback: false //tells nextjs to show 404 if param is not matched
+    }
 }
 
 export default Product
